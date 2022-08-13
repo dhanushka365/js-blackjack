e709c7cf8f586aba13a2b1eb0de0e7cec447a244
uduwela96 <puduwel@mitrai.com>
2022-08-13 16:51:06 +0530

    creating new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './home/home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
